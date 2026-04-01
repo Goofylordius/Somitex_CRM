5a3352558acc2a19e86bb2bf5959ef69d3d160a0
Malte Harry Schendel <schendem@tuv.academy>
2026-04-01 13:34:16 +0200

    Fix root page typed redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function HomePage() {
-  const headerStore = await headers();
-  const host = headerStore.get("x-forwarded-host") ?? headerStore.get("host");
-  const protocol = headerStore.get("x-forwarded-proto") ?? "https";
-
-  if (host) {
-    redirect(new URL("/index.html", `${protocol}://${host}`));
-  }
-
+export default function HomePage() {
   redirect("/login");
 }
